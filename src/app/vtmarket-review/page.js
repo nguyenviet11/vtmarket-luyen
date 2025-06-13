a26717a669c7fbe94923976de27edd2c394c7796
vietnv-mmo <nguyenvanvietbn1998@gmail.com>
2025-06-13 15:59:35 +0700

    uppp

--- src/app/vtmarket-review/page.js
+++ src/app/vtmarket-review/page.js
@@ -1,7 +1,7 @@
 'use client'
 import styles from "./page.module.css";
-import circel from "../../public/R-tron.png";
-import image from "../../public/image.png";
+import circel from "../../../public/R-tron.png";
+import image from "../../../public/image.png";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
