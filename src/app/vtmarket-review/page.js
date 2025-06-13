05005de3921587e5f8085b4cf749ee38a1852e83
vietnv-mmo <nguyenvanvietbn1998@gmail.com>
2025-06-13 15:55:57 +0700

    uppp

--- src/app/vtmarket-review/page.js
+++ src/app/vtmarket-review/page.js
@@ -18,7 +18,7 @@ export default function Home() {
     >
       <main className={styles.main}>
         <h3 className={styles.text}>
-        VT Markets Official Site{" "}
+        VT Markets Official Site{""}
           <span
             style={{ transform: "translate(0px)", display: "inline-block" }}
           >
